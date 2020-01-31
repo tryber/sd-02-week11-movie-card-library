40a5f81bc6e6134abf3cdb4e6bbb344d4b4e805a
lauro <laurolyra@gmail.com>
2020-01-31 18:41:55 -0300

    aprovação em todos os testes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,1 +1,11 @@
-// Iniciando o projeto 🚀
+import React from 'react';
+
+export default class Header extends React.Component{
+  render() {
+    return (
+      <header>
+        <h1>Movie Cards Library</h1>
+      </header>
+    )
+  }
+}
--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,17 @@
-// implement MovieCard component here
+import React from 'react';
+import Rating from './Rating'
+
+export default class MovieCard extends React.Component {
+  render() {
+    const { title, subtitle, storyline, imagePath, rating } = this.props.movie
+    return (
+      <div className='movieDiv'>
+        <h4>{title}</h4>
+        <h5>{subtitle}</h5>
+        <p>{storyline}</p>
+        <Rating rating = {rating}/>
+        <img src = {imagePath}/>
+      </div>
+    )
+  }
+}

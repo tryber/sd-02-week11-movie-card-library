5f36a1361b636ec490f30a069813341e0047b73d
guicgs <etohlif@protonmail.com>
2020-01-31 00:20:55 -0300

    requisitos concluídos e classes do CSS implementadas

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,13 +1,17 @@
 import React from 'react';
+import Rating from './Rating';
 
 class MovieCard extends React.Component {
   render() {
     const { title, subtitle, storyline, imagePath, rating } = this.props.movie;
 
     return (
-      <div>
-        <img src={imagePath} />
-        <h4>{title}</h4>
+      <div className="movie-card movie-card-body">
+        <img src={imagePath} className="movie-card-image" alt="imagem do filme" />
+        <h4 className="movie-card-title">{title}</h4>
+        <h5 className="movie-card-subtitle">{subtitle}</h5>
+        <p className="movie-card-storyline">{storyline}</p>
+        <Rating rating={rating} className="movie-card-rating" />
       </div>
     )
   }

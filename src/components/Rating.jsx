07a26a7ee1d035740559c7287853cb87727634f3
Douglas Henrique <doughrqe@gmail.com>
2020-01-31 13:02:54 -0300

    Code CLimate

--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
+
 class Rating extends React.Component {
-	render() {
-		const { rating } = this.props;
-		return (
-			<div className="movie-card-rating">
-				<h6 className="rating">{rating}</h6>
-			</div>
-		);
-	}
+  render() {
+    const rating = this.props;
+    return (
+      <div className="movie-card-rating">
+        <h6 className="rating">{rating}</h6>
+      </div>
+    );
+  }
 }
 export default Rating;

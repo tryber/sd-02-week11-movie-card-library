d64407bdd46d889fec16dcc12f9104da9dcc1648
Lucas-Werner <lucas.1werner@gmail.com>
2020-01-31 00:12:19 -0300

    requisitos minimos

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,19 +1,18 @@
-// implement MovieCard component here
 import React from 'react';
 
 class MovieCard extends React.Component {
-  render () {
-    const {title, subtitle, storyline, imagePath, rating} = this.props.movies
+  render() {
+    const { title, subtitle, storyline, imagePath, rating } = this.props.movies
     return (
-    <div className="moviecard">
-      <img src={imagePath}/>
-      <h4>{title}</h4>
-      <h5>{subtitle}</h5>
-      <p>{storyline}</p>
-      <Rating rating={rating}/>
-    
-    
-    </div>
+      <div className="moviecard">
+        <img src={imagePath} />
+        <h4>{title}</h4>
+        <h5>{subtitle}</h5>
+        <p>{storyline}</p>
+        <Rating rating={rating} />
+      </div>
     )
-  }
-}
+  };
+};
+
+export default MovieCard
--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -10,7 +10,7 @@ class Rating extends React.Component {
         </p>
       </div>
     )
-  }
-}
+  };
+};
 
 export default Rating

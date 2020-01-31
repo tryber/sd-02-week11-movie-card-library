6410a2e866893032009fc768862962f0319b7f4b
phtf92 <phtf92@gmail.com>
2020-01-31 11:34:58 -0300

    exportando os dados para a página

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,18 @@
-// implement MovieCard component here
+import React from 'react';
+
+class MovieCard extends React.Component {
+    render() {
+        const { title, subtitle, storyline, rating, imagePath } = this.props.filme;
+        return (
+            <div>
+                <p>{title}</p>
+                <p>{subtitle}</p>
+                <p>{storyline}</p>
+                <p>{rating}</p>
+                <p>{imagePath}</p>
+            </div>
+        )
+    }
+}
+
+export default MovieCard
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,13 @@
-// implement MovieList component here
+import React from 'react';
+import MovieCard from './MovieCard'
+
+class MovieList extends React.Component {
+    render() {
+        const listaFilmes = this.props.movies
+        return (<div className='movieList'>
+            {listaFilmes.map(filme => <MovieCard filme={filme} />)}
+        </div>);
+            }
+        }
+        
+        export default MovieList;// implement MovieList component here

a19789d757cc8d9a94548e03dfd87bfce3ea4ffc
Johnatas Henrique <johnatas.henrique@gmail.com>
2020-01-31 17:28:46 -0300

    MovieList pronta

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,18 @@
 // implement MovieCard component here
+import React from 'react';
+
+class MovieCard extends React.Component {
+    render() {
+        const {title, subtitle, storyline, rating, imagePath} = this.props;
+        return (
+            <div className="movie-card">
+                <img className="movie-card-image" src={imagePath} alt={`${subtitle} - ${title}`}></img>
+                <h4 className="movie-card-title">{title}</h4>
+                <h5 className="movie-card-subtitle">{subtitle}</h5>
+                <p className="movie-card-storyline">{storyline}</p>
+            </div>
+        );
+    }
+}
+
+export default MovieCard;
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,18 @@
 // implement MovieList component here
+import React from 'react';
+import MovieCard from './MovieCard';
+
+class MovieList extends React.Component {
+    render() {
+        return (
+            <div className="movie-list">
+                {this.props.movies.map(item => 
+                <MovieCard key={item.title} title={item.title}
+                subtitle={item.subtitle} storyline={item.storyline} 
+                rating={item.rating} imagePath={item.imagePath} />)}
+            </div>
+        );
+    }
+}
+
+export default MovieList;

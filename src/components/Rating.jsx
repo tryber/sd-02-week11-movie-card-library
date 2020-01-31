6aefc7f5f3f8a8a584d0dc6425c3ea3c90cc72a5
Douglas Henrique <doughrqe@gmail.com>
2020-01-31 13:25:18 -0300

    Bug Fix

--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 class Rating extends React.Component {
   render() {
-    const rating = this.props;
+    const {rating} = this.props;
     return (
       <div className="movie-card-rating">
         <h6 className="rating">{rating}</h6>

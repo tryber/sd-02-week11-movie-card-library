65fc13649346049be34d3735526b82abf819aecd
EddyeBoy27 <lipe0503@gmail.com>
2020-01-31 19:05:21 -0200

    CC Issues Fix

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -7,10 +7,11 @@ class MovieList extends React.Component {
   render() {
     return (
       <div className="movie-list">
-        {this.props.movies.map(movie => (
+        {this.props.movies.map((movie) => (
           <MovieCard
-          key={movie.title}
-          movie={movie} />
+            key={movie.title}
+            movie={movie}
+          />
         ))}
       </div>
     );

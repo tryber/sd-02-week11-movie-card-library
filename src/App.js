632f21d69178b07cc1d7186e4f438544be7f535d
Miguel Miranda <ayandram7727@gmail.com>
2020-01-31 19:11:37 -0200

    passou em todos os testes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import './App.css';
+import Header from './components/Header.jsx';
+import MovieList from './components/MovieList.jsx';
+import movies from './data.js';
 
 function App() {
   return (
     <div className="App">
-      {/* Sua implementação deve ficar aqui. Remova essas duas linhas e mão na massa */}
+      <Header />
+      <MovieList movies={movies} />
     </div>
   );
 }

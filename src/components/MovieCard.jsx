0c9f5b0debfa6a4048832f22c83438c32bd3ea84
phtf92 <phtf92@gmail.com>
2020-01-31 16:55:18 -0300

    Code cliamte

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -7,12 +7,12 @@ class MovieCard extends React.Component {
     return (
       <div className="movie-card">
         <div className="movie-card-body">
-            <img className="movie-card-image" src={imagePath} alt={title} />
-            <h4 className="movie-card-title">{title}</h4>
-            <h5 className="movie-card-subtitle">{subtitle}</h5>
-            <p className="movie-card-storyline">{storyline}</p>
-            <Rating rating={rating} />
-          </div>
+          <img className="movie-card-image" src={imagePath} alt={title} />
+          <h4 className="movie-card-title">{title}</h4>
+          <h5 className="movie-card-subtitle">{subtitle}</h5>
+          <p className="movie-card-storyline">{storyline}</p>
+          <Rating rating={rating} />
+        </div>
       </div>
     );
   }

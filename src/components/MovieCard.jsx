806ac48a989ce67b2bd36ced36cd29b60792d724
guicgs <etohlif@protonmail.com>
2020-01-30 23:43:31 -0300

    requisitos 3 e 4 concluídos

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,15 @@
-// implement MovieCard component here
+import React from 'react';
+
+class MovieCard extends React.Component {
+  render() {
+    const movies = this.props.movies;
+
+    return (
+      <div>
+        {movies.map((movie, i) => <MovieCard key={i} movie={movie} />)}
+      </div>
+    )
+  }
+}
+
+export default MovieCard;

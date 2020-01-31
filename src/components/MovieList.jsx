f21bcac65ecfd86134de1eb236c9b65f35f6c1c9
Andrey <andreytsuzuki@gmail.com>
2020-01-31 15:46:44 -0300

    fix

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -6,13 +6,13 @@ import MovieCard from './MovieCard';
 export default class MovieList extends React.Component {
   render() {
     return (
-      <div className='movie-list'>
-        {this.props.movies.map(movie =>
+      <div className="movie-list">
+        {this.props.movies.map(movie => (
           <MovieCard
             key={movie.title}
             movie={movie}
           />
-        )}
+        ))};
       </div>
     );
   }

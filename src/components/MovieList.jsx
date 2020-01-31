f37568656af1381bda5aa95b36c5a44bf2bf2118
Johnatas Henrique <johnatas.henrique@gmail.com>
2020-01-31 17:32:42 -0300

    CodeClimate - component MovieList

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -3,16 +3,15 @@ import React from 'react';
 import MovieCard from './MovieCard';
 
 class MovieList extends React.Component {
-    render() {
-        return (
-            <div className="movie-list">
-                {this.props.movies.map(item => 
-                <MovieCard key={item.title} title={item.title}
-                subtitle={item.subtitle} storyline={item.storyline} 
-                rating={item.rating} imagePath={item.imagePath} />)}
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div className="movie-list">
+        {this.props.movies.map(item => 
+          <MovieCard key={item.title} title={item.title} subtitle={item.subtitle}
+          storyline={item.storyline} rating={item.rating} imagePath={item.imagePath} />)}
+      </div>
+    );
+  }
 }
 
 export default MovieList;

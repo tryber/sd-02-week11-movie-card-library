0349ae0ae738594e3b8fbee89e042780da4f47fc
Douglas Henrique <doughrqe@gmail.com>
2020-01-31 12:53:44 -0300

    Adequação Code Climate

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,15 +1,15 @@
 import React from 'react';
-import MovieCard from './MovieCard'
+import MovieCard from './MovieCard';
 
 class MovieList extends React.Component {
-    render() {
-        const movies = this.props.movies;
+  render() {
+    const movies = this.props.movies;
 
-        return (
-            <div className="movie-list" >
-                {movies.map((movie) =>  <MovieCard key={movie.title} movie = { movie } />)}
-            </div>
-        );
-    }
+    return (
+      <div className="movie-list" >
+        {movies.map((movie)=> <MovieCard key={movie.title} movie={movie} />)}
+      </div>
+    );
+  }
 }
 export default MovieList;

9da35717355fcf74f83782ef168cee8d0c62f03d
Andrey <andreytsuzuki@gmail.com>
2020-01-31 16:08:26 -0300

    fix

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -7,12 +7,14 @@ export default class MovieCard extends React.Component {
     const { title, imagePath, subtitle, storyline, rating } = this.props.movie;
     return (
       <div className="movie-card">
-        <h4 className="movie-card-title">{title}</h4>
+        <img className="movie-card-image" src={imagePath} alt="" />
         <div className="movie-card-body">
+          <h4 className="movie-card-title">{title}</h4>
           <h5 className="movie-card-subtitle">{subtitle}</h5>
-          <img className="movie-card-image" src={imagePath} alt="" />
           <p className="movie-card-storyline">{storyline}</p>
-          <Rating className="movie-card-rating" rating={rating} />
+        </div>
+        <div className="movie-card-rating">
+          <Rating rating={rating} />
         </div>
       </div>
     );

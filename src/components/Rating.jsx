0a05330b444f897a7a9b4891a9ca20fd8a9f6072
Johnatas Henrique <johnatas.henrique@gmail.com>
2020-01-31 18:29:48 -0300

    CodeClimate - componente Rating

--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -2,13 +2,13 @@
 import React from 'react';
 
 class Rating extends React.Component {
-    render() {
-        return (
-          <div className="rating">
-            {this.props.rating}
-          </div>
-        );
-    }
+  render() {
+    return (
+      <div className="rating">
+        {this.props.rating}
+      </div>
+    );
+  }
 }
 
 export default Rating;

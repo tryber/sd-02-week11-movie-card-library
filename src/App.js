f9df6a9d886a949dff9c58d0b7b2a9295eb2f8f6
Julio Cezar Taveira Araujo <jctaraujo@hotmail.com>
2020-01-31 11:26:35 -0200

    Finalizado Projeto

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
 import React from 'react';
 import './App.css';
 import Header from './components/Header';
-// import MovieCard from './components/MovieCard';
-// import MovieList from './components/MovieList';
-// import Rating from './components/Rating';
+import MovieList from './components/MovieList';
+import movies from './data';
 
 function App() {
   return (
-    <div className="App">
-      {/* Sua implementação deve ficar aqui. Remova essas duas linhas e mão na massa */}
-      Hora de codar! 🚀✍️👨‍💻👩‍💻💪
+    <div>
+      <Header />
+      <MovieList movies={movies} />
     </div>
   );
 }

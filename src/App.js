33efff3105c8e3b35e15584b616ca38538e4abfc
Douglas Henrique <doughrqe@gmail.com>
2020-01-31 13:08:34 -0300

    Code CLimate

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
   return (
     <div className="App">
       <Header />
-      <MovieList movies={movies}/>
+      <MovieList movies={movies} />
     </div>
   );
 }
--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -8,7 +8,7 @@ class MovieCard extends React.Component {
     return (
       <div className="movie-card">
         <div className="movie-card-body">
-          <img className="movie-card-image" src={imagePath} alt="Imagem do Filme"/>
+          <img className="movie-card-image" src={imagePath} alt="Imagem do Filme" />
           <h4 className="movie-card-title">{title}</h4>
           <h5 className="movie-card-subtitle">{subtitle}</h5>
           <p className="movie-card-storyline">{storyline}</p>

f2b43ea6ddfda537325dbc383f91db6f8c4257a4
Julio Cezar Taveira Araujo <jctaraujo@hotmail.com>
2020-01-31 09:44:58 -0200

    Iniciando o projeto

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
 import React from 'react';
 import './App.css';
+import Header from './components/Header';
+// import MovieCard from './components/MovieCard';
+// import MovieList from './components/MovieList';
+// import Rating from './components/Rating';
 
 function App() {
   return (

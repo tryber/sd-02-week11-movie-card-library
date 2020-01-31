128a7d99cc95de0ab53e27f5c0cfcb411145da9a
Fernando Luckesi <fernando.luckesi@gmail.com>
2020-01-31 19:40:41 -0300

    projeto iniciado

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,20 @@
 // implement MovieCard component here
+import React from 'react';
+import Rating from './Rating';
+import movies from '../data';
+
+class MovieCard extends React.Component {
+    render() {
+        const { title, subtitle, storyline, rating, imagePath } = this.props.movie
+        return (
+            <div className="movie-card">
+                <img className="movie-card-image" src={imagePath} alt={title} />
+                <h4>{title}</h4>
+                <Rating />
+            </div>
+        )
+
+    }
+}
+
+export default MovieCard;

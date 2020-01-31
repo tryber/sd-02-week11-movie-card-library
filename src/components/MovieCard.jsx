da66ca4c61efd18b43d627cfe8fc8cea2b3800ef
lauro <laurolyra@gmail.com>
2020-01-31 18:59:20 -0300

    adequações ao CC

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -5,12 +5,12 @@ export default class MovieCard extends React.Component {
   render() {
     const { title, subtitle, storyline, imagePath, rating } = this.props.movie;
     return (
-      <div className = "movieDiv">
+      <div className="movieDiv">
         <h4>{title}</h4>
         <h5>{subtitle}</h5>
         <p>{storyline}</p>
         <Rating rating={rating} />
-        <img src={imagePath} alt = "movie cover" />
+        <img src={imagePath} alt="movie cover" />
       </div>
     );
   }
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -6,7 +6,7 @@ export default class MovieList extends React.Component {
     const movies = this.props.movies;
     return (
       <div className="movie-list">
-        {movies.map((movie) => <MovieCard key={movie.title} movie={movie}/>) }
+        {movies.map((movie) => <MovieCard key={movie.title} movie={movie} />) }
       </div>
     );
   }

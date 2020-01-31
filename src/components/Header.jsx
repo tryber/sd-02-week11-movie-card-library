5ed0d0b16d7077fdc910b8fb4fecc3d0fd0ed4ed
Lucas-Werner <lucas.1werner@gmail.com>
2020-01-30 23:25:47 -0300

    dois primeiros requisitos

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,10 +2,11 @@ import React from 'react'
 
 class Header extends React.Component {
   render() {
+    return (
     <header>
-      <h1>Movie Card!</h1>
+      <h1>Movie Cards Library</h1>
     </header>
-  }
+    )
 }
-
+}
 export default Header
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,7 @@
-// implement MovieList component here
+import React from 'react'
+
+class Movie extends React.Component {
+  render () {
+    const
+  }
+}

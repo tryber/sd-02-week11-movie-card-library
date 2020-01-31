aa50b893edaccd842160e862e6ca24d847100035
Douglas Henrique <doughrqe@gmail.com>
2020-01-30 23:07:28 -0300

    renderizando movie card e movie list

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,15 @@
-// implement MovieCard component here
+import React from 'react';
+class MovieCard extends React.Component {
+    render() {
+        const { tittle, subtitle, storyline, imagePath } = this.props.movie;
+        return (
+            <div>
+                <img src={imagePath} />
+                <h4>{tittle}</h4>
+                <h5>{subtitle}</h5>
+                <p>{storyline}</p>
+            </div>
+        )
+    }
+}
+export default MovieCard;

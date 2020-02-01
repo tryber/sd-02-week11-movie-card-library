05cc76e3e727c0a03214f4f11a082db21d5cc7d9
Giorgia <giorgialeoniac@gmail.com>
2020-01-31 21:15:10 -0300

    Correções code climate parte 2 - A volta dos que não foram (indentados)

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 import MovieCard from './MovieCard';
 
 class MovieList extends React.Component {
-    render() {
-      const movies = this.props.movies;
-      return (
+  render() {
+    const movies = this.props.movies;
+    return (
         <div className="listing">
           {movies.map((movie) => <MovieCard key={movie.title} movie={movie} />)}
         </div>
--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 
 class Rating extends React.Component {
-    render() {
-      const { rating } = this.props;
-      return (
+  render() {
+    const { rating } = this.props;
+    return (
           <div className="movies-ratings">
             <h6 className="rating">{rating}</h6>
           </div>
-        );
-    }
+    );
+  }
 }
 
 export default Rating;

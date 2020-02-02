8e5489321b2e6eb23ce734134707ea380118d788
Fernando Luckesi <fernando.luckesi94@gmail.com>
2020-02-02 00:39:47 -0300

    Code Climate version 1.7

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,8 +5,8 @@ class Header extends React.Component {
   render() {
     return (
       <header className="movie-card-header">
-          <h1 className="page-title">Movie Cards Library</h1>
-        </header>
+        <h1 className="page-title">Movie Cards Library</h1>
+      </header>
     );
   }
 }

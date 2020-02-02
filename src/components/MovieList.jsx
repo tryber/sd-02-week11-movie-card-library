5932d9b91300b3c6dd3f33de758acdde4cda32ec
Fernando Luckesi <fernando.luckesi94@gmail.com>
2020-02-01 23:47:29 -0300

    Code Climate version 1.2

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -3,13 +3,13 @@ import React from 'react';
 import MovieCard from './MovieCard';
 
 class MovieList extends React.Component {
-    render() {
-        return (
-            <div className="movie-list">
-                {this.props.movies.map(item => 
-                <MovieCard key={item.title} movie={item} />)}            
-             </div>
-        )
+  render() {
+      return (
+          <div className="movie-list">
+              {this.props.movies.map((item) => 
+              <MovieCard key={item.title} movie={item} />)}         
+          </div>
+        );
     }
 }
 

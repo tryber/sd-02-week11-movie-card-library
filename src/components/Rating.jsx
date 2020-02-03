4620914a3fdf5ee6660217eecd1f1155442d6a00
Ishidor <kevinmendessilva@hotmail.com>
2020-02-03 11:47:33 -0300

    Salvando o projeto

--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,1 +1,10 @@
 // implement Rating component here
+import React from 'react';
+
+export default class Rating extends React.Component {
+  render() {
+    return (
+      <p className="rating">{this.props.rating}</p>
+    );
+  }
+}
